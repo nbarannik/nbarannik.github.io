5f8435fb5a82a43b79c9b3c550cb4c7b2b2a572d
nbarannik <48026764+nbarannik@users.noreply.github.com>
2019-12-31 15:17:02 +0300

    1.1

--- BuyPage/js/MoneyCounter.js
+++ BuyPage/js/MoneyCounter.js
@@ -2,11 +2,8 @@ let robloxInp = document.querySelector('#MoneyInp');
 let NickInp = document.querySelector('#NicknameInp');
 let robloxTxt = document.querySelector('#roblox');
 let rublesTxt = document.querySelector('#rubles');
-let roblox = 0;
-let money = robloxInp / 2.1;
-money.toPrecision(1);
 
 robloxInp.addEventListener('keyup', function(e) {
   robloxTxt.textContent = e.target.value;
-  rublesTxt.textContent = Math.round(e.target.value / 2.1);
+  rublesTxt.textContent = e.target.value / 2;
 })
