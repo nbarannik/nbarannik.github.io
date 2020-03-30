2eac9c889a7a59271650b060685969d65a2b3a4b
nbarannik <48026764+nbarannik@users.noreply.github.com>
2020-03-30 15:00:46 +0300

    111

--- BuyPage/js/MenuScript.js
+++ BuyPage/js/MenuScript.js
@@ -6,10 +6,12 @@ let isOpened = false;
 function MenuAnim() {
   if(isOpened == false){
     menu.classList.add('menu-content-active');
+    button.classList.add('m-menu-btn-active');
     isOpened = true;
   }
   else{
     menu.classList.remove('menu-content-active');
+    button.classList.remove('m-menu-btn-active');
     isOpened = false;
   }
 
