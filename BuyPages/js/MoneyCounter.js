f7e359f7aab096b733a5842b33dff67843912423
nbarannik <48026764+nbarannik@users.noreply.github.com>
2020-03-30 10:24:03 +0300

    12

--- BuyPages/js/MoneyCounter.js
+++
@@ -1,9 +0,0 @@
-let robloxInp = document.querySelector('#MoneyInp');
-let NickInp = document.querySelector('#NicknameInp');
-let robloxTxt = document.querySelector('#roblox');
-let rublesTxt = document.querySelector('#rubles');
-
-robloxInp.addEventListener('keyup', function(e) {
-  robloxTxt.textContent = e.target.value;
-  rublesTxt.textContent = e.target.value / 2;
-})
